f94e1e4cc82a898bdfc5476dd990bebf6f4be11c
Idris802 <77642526+Idris802@users.noreply.github.com>
2022-12-04 16:56:46 +0100

    ImageHandler uses old read file.

---
+++ src/LossLess/read.h
@@ -0,0 +1,15 @@
+#ifndef PIXELSTOCIRCLES_READ_H
+#define PIXELSTOCIRCLES_READ_H
+
+#include <cassert>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+#include "read.cpp"
+
+std::vector<std::vector<int>> get_vect(std::string filename);
+
+#endif //PIXELSTOCIRCLES_READ_H
