6b1181a0947b9eabd1f84b8d9c215457bf1ae6c4
Idris802 <77642526+Idris802@users.noreply.github.com>
2022-12-05 22:03:18 +0100

    open MP with dual processing.

--- src/LossLess/ImageHandler.cpp
+++ src/LossLess/ImageHandler.cpp
@@ -1,45 +1,70 @@
 #include <iostream>
 #include "ImageHandler.h"
-
-#include <fstream>
-#include <vector>
-#include <string>
-
-
-void DoRandomProcess() {
-	ImageHandler MyImage; // Create an object for the image
-        MyImage.FileName = "../benchmark.0032.pxl";
-	MyImage.BackGround = 0;
-	MyImage.DiskColor = 1;
+#include <omp.h>
+#include <unistd.h>
+#include <chrono>
 
 
-        MyImage.Image = get_vect(MyImage.FileName);
-        MyImage.PrintImage(MyImage.Image, "Original Image");
+void DoRandomProcess(std::string path) {
+        ImageHandler MyImage; // Create an object for the image
+        MyImage.FileName = path;
+        MyImage.BackGround = 0;
+        MyImage.DiskColor = 1;
 
-        MyImage.OwnMap = MyImage.CreateMap(MyImage.Image);
-	MyImage.GenerateRandomCircles();
-
-}
-
-void DoPairProcess() {
-	ImageHandler MyImage; // Create an object for the image
-        MyImage.FileName = "../benchmark.0032.pxl";
-	MyImage.BackGround = 0;
-	MyImage.DiskColor = 1;
-	MyImage.WriteTo = "output-disk-data.dat";
 
         MyImage.Image = get_vect(MyImage.FileName);
         MyImage.PrintImage(MyImage.Image, "Original Image");
 
         MyImage.OwnMap = MyImage.CreateMap(MyImage.Image);
-	MyImage.DrawFromPairs();
-}
-	
-int main() {
+        MyImage.GenerateRandomCircles();
 
-        DoRandomProcess();
-	DoPairProcess();
+}
+
+void DoPairProcess(std::string path) {
+        ImageHandler MyImage; // Create an object for the image
+        MyImage.FileName = path;
+        MyImage.BackGround = 0;
+        MyImage.DiskColor = 1;
+        MyImage.WriteTo = "output-disk-data.dat";
+
+        MyImage.Image = get_vect(MyImage.FileName);
+        MyImage.PrintImage(MyImage.Image, "Original Image");
+
+        MyImage.OwnMap = MyImage.CreateMap(MyImage.Image);
+        MyImage.DrawFromPairs();
+}
+
+
+int main(int argc, char** argv) {
+
+        omp_set_num_threads(2);
+        std::string path = argv[1];
+
+
+
+        auto t0 = std::chrono::high_resolution_clock::now();
+
+        std::chrono::time_point<std::chrono::system_clock, std::chrono::duration<long, std::ratio<1, 1000000000>>> t1;
+
+        std::chrono::time_point<std::chrono::system_clock, std::chrono::duration<long, std::ratio<1, 1000000000>>> t2;
+
+
+        #pragma omp parallel
+        {
+        if(omp_get_thread_num() == 0){
+                DoPairProcess(path);
+                t1 = std::chrono::high_resolution_clock::now();
+                }
+
+        if(omp_get_thread_num() == 1){
+                DoRandomProcess(path);
+                t2 = std::chrono::high_resolution_clock::now();
+                }
+        }
+        auto time_pair = std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count()/std::pow(10, 6);
+        std::cout << "Time to do pairs = " << time_pair << "ms" << std::endl;
+                auto time_random = std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t0).count()/std::pow(10, 6);
+        std::cout << "Time to do Random = " << time_random << "ms" << std::endl;
 
         return 0;
 }
-
