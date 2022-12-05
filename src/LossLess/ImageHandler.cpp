c95886d1a0fad8153b40fbb0c073024e8790e491
Idris802 <77642526+Idris802@users.noreply.github.com>
2022-12-05 16:04:01 +0100

    Update ImageHandler.cpp

--- src/LossLess/ImageHandler.cpp
+++ src/LossLess/ImageHandler.cpp
@@ -1,36 +1,44 @@
+#include <iostream>
+#include "ImageHandler.h"
+
+#include <fstream>
+#include <vector>
+#include <string>
+
+
 void DoRandomProcess() {
-        ImageHandler MyImage; // Create an object for the image
+	ImageHandler MyImage; // Create an object for the image
         MyImage.FileName = "../benchmark.0032.pxl";
-        MyImage.BackGround = 0;
-        MyImage.DiskColor = 1;
+	MyImage.BackGround = 0;
+	MyImage.DiskColor = 1;
 
 
         MyImage.Image = get_vect(MyImage.FileName);
         MyImage.PrintImage(MyImage.Image, "Original Image");
 
         MyImage.OwnMap = MyImage.CreateMap(MyImage.Image);
-        MyImage.GenerateRandomCircles();
+	MyImage.GenerateRandomCircles();
 
 }
 
 void DoPairProcess() {
-        ImageHandler MyImage; // Create an object for the image
+	ImageHandler MyImage; // Create an object for the image
         MyImage.FileName = "../benchmark.0032.pxl";
-        MyImage.BackGround = 0;
-        MyImage.DiskColor = 1;
-        MyImage.WriteTo = "output-disk-data.dat";
+	MyImage.BackGround = 0;
+	MyImage.DiskColor = 1;
+	MyImage.WriteTo = "output-disk-data.dat";
 
         MyImage.Image = get_vect(MyImage.FileName);
         MyImage.PrintImage(MyImage.Image, "Original Image");
 
         MyImage.OwnMap = MyImage.CreateMap(MyImage.Image);
-        MyImage.DrawFromPairs();
+	MyImage.DrawFromPairs();
 }
-
+	
 int main() {
 
         DoRandomProcess();
-        DoPairProcess();
+	DoPairProcess();
 
         return 0;
 }
